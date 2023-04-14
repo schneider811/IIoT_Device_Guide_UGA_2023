23f0c0f8a8afc5247a5161d8838fc7987369c8ef
Keith Schneider <keith.schneider811@gmail.com>
2023-04-14 16:28:24 -0400

    fixing reboot

--- all_together/all_together.sh
+++ all_together/all_together.sh
@@ -1,3 +1,3 @@
 chmod +x /home/pi/IIoT_Device_Guide_UGA_2023/all_together/dist/all_together
-crontab -l | { cat; echo "* * * * * @reboot /home/pi/IIoT_Device_Guide_UGA_2023/all_together/dist/all_together 2>&1"; } | crontab -
+crontab -l | { cat; echo "* * * * * /home/pi/IIoT_Device_Guide_UGA_2023/all_together/dist/all_together 2>&1"; } | crontab -
 crontab -l | { cat; echo "@reboot /home/pi/IIoT_Device_Guide_UGA_2023/all_together/dist/all_together 2>&1"; } | crontab -
