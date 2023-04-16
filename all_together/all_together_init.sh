b086cb702b199b9697d9d61472c13a28d0150bb1
Keith Schneider <keith.schneider811@gmail.com>
2023-04-16 01:04:07 -0400

    renaming

--- all_together/all_together_init.sh
+++ all_together/all_together_init.sh
@@ -1,4 +1,4 @@
-chmod +x /home/pi/IIoT_Device_Guide_UGA_2023/all_together/dist/all_together
+chmod +x /home/pi/IIoT_Device_Guide_UGA_2023/all_together/dist/built_file
 chmod +x /home/pi/IIoT_Device_Guide_UGA_2023/all_together/running.sh
 echo "* * * * * /home/pi/IIoT_Device_Guide_UGA_2023/all_together/running.sh 2>&1" | crontab -
 crontab -l | { cat; echo "@reboot /home/pi/IIoT_Device_Guide_UGA_2023/all_together/running.sh 2>&1"; } | crontab -
--- all_together/running.sh
+++ all_together/running.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 now=$(date +"%T")
-SERVICE="all_together"
+SERVICE="built_file"
 ADCDRIVER="/home/pi/IIoT_Device_Guide_UGA_2023/all_together/dist"
 if pgrep -f "$SERVICE" 
 then
